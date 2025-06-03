4dfac9ff0f2551a17129359483b80f23013c1ca3
Kwright6 <usaf633@yahoo.com>
2025-06-03 13:04:36 +0000

    Further Implemented Execute function

--- inventoryCmd.cpp
+++ inventoryCmd.cpp
@@ -6,9 +6,24 @@ using namespace std;
 
 InventoryCmd::InventoryCmd() : cmd('I') {} // Constructor
 
-void InventoryCmd::execute(MovieStore& store) const {
+void InventoryCmd::execute(MovieStore &store) const {
   // display store inventory information by calling function with
   // passed movie store object
+  static const int tablesize = 'Z' - 'A' + 1;
+  const auto &inventory = store.getMoviesByType();
+  for (int i = 0; i < tablesize; i++) {
+    const auto &movieList = inventory[i];
+    if (!movieList.empty()) {
+      for (const Movie *movie : movieList) {
+        if (movie != nullptr) {
+          // print movies
+          cout << "execute[I]: " << movie->stock << ", " << movie->director
+               << ", " << movie->title << endl;
+          // movie->print(); (TODO)
+        }
+      }
+    }
+  }
 }
 
 InventoryCmdFactory::InventoryCmdFactory() { registerType('I', this); }
