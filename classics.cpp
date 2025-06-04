c5ff8acce743cce14b1eec3f54d1869c08c97f4e
Kwright6 <usaf633@yahoo.com>
2025-06-04 09:21:57 +0000

    Implemented Classics

---
+++ classics.cpp
@@ -0,0 +1,43 @@
+#include "classics.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+Classics::Classics(const vector<string> &vs) {
+  if (vs.size() == 5) {
+    // Initialize variables from vector<string>
+    genre = vs[0][0];
+    stock = stoi(vs[1]);
+    director = vs[2];
+    title = vs[3];
+    // Use sstream to get variables separated by spaces in vs[4]
+    istringstream iss(vs[4]);
+    string firstName;
+    string lastName;
+    int mo;
+    int yr;
+    iss >> firstName >> lastName >> mo >> yr;
+    // Initialize variables
+    majorActor = firstName + " " + lastName;
+    month = mo;
+    year = yr;
+  }
+}
+
+void Classics::print() const {
+  // C, 10, Victor Fleming, The Wizard of Oz, Judy Garland 7 1939
+  // 1939 7, Judy Garland, Victor Fleming, The Wizard of Oz (10) - Classics
+  // year month, majorActor, Director, Title (stock) - Classics
+  cout << year << " " << month << ", " << majorActor << ", " << director << ", "
+       << title << " (" << stock << ")" << " - Classics" << endl;
+}
+
+ClassicsFactory::ClassicsFactory() { registerType('C', this); }
+
+Movie *ClassicsFactory::makeMovie(const vector<string> &vs) const {
+  return new Classics(vs);
+}
+
+// creating the object registers the type at run time
+ClassicsFactory theClassicsFactory;
---
+++ classics.h
@@ -0,0 +1,37 @@
+#ifndef CLASSICS_H
+#define CLASSICS_H
+
+/**
+ * Classics class, Classics are Movies
+ */
+
+#include "movie.h"
+#include "movieFactory.h"
+#include <vector>
+
+class Classics : public Movie {
+    private:
+        char genre; // "C"
+    public:
+        int month;
+        int year;
+        string majorActor;
+        
+        // Constructor
+        explicit Classics(const vector<string> &vs);
+
+        void print() const override;
+};
+
+/**
+ * Creating ClassicFactory to make Classic objects
+ * ClassicFactory object will register itself later and get stored in Movie class
+ */
+
+ class ClassicsFactory : public MovieFactory {
+    public:
+        ClassicsFactory(); // constructor
+        Movie *makeMovie(const vector<string> &vs) const override;
+ };
+
+#endif // CLASSICS_H
