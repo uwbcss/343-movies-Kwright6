e47a660150e77222a6c08c5b32b377b4581d9891
Kwright6 <usaf633@yahoo.com>
2025-06-04 08:22:52 +0000

    Implemented comedy

---
+++ comedy.cpp
@@ -0,0 +1,32 @@
+#include "comedy.h"
+#include <iostream>
+
+using namespace std;
+
+Comedy::Comedy(const vector<string> &vs) {
+  if (vs.size() == 5) {
+    // Initialize variables from vector<string>
+    genre = vs[0][0];
+    stock = stoi(vs[1]);
+    director = vs[2];
+    title = vs[3];
+    year = stoi(vs[4]);
+  }
+}
+
+void Comedy::print() const {
+  // F, 10, Nora Ephron, You've Got Mail, 1998
+  // You've Got Mail, 1998, Nora Ephron (10) - Comedy
+  // Title, Year, Director (stock) - Comedy
+  cout << title << ", " << year << ", " << director << " (" << stock << ")"
+       << " - Comedy" << endl;
+}
+
+ComedyFactory::ComedyFactory() { registerType('F', this); }
+
+Movie *ComedyFactory::makeMovie(const vector<string> &vs) const {
+  return new Comedy(vs);
+}
+
+// creating the object registers the type at run time
+ComedyFactory theComedyFactory;
---
+++ comedy.h
@@ -0,0 +1,34 @@
+#ifndef COMEDY_H
+#define COMEDY_H
+
+/**
+ * Comedy class, Comedy is a Movie
+ */
+
+#include "movie.h"
+#include "movieFactory.h"
+#include <vector>
+
+class Comedy : public Movie {
+    private:
+        char genre; // 'F'
+    public:
+        int year;
+
+        // Constructor
+        explicit Comedy(const vector<string>& vs);
+
+        void print() const override;
+};
+
+/**
+ * Creating ComedyFactory to make Comedy objects
+ * ComedyFactory object will register itself later and get stored in Movie class
+ */
+class ComedyFactory : public MovieFactory {
+public:
+  ComedyFactory();
+  Movie *makeMovie(const vector<string>& vs) const override;
+};
+
+#endif // COMEDY_H
