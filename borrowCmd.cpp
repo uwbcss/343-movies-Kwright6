c6b659ed46ab4460088f0b0d7047bf6e071ce13b
Kwright6 <usaf633@yahoo.com>
2025-06-05 13:39:27 +0000

    Implementing Borrow

--- borrowCmd.cpp
+++ borrowCmd.cpp
@@ -8,59 +8,192 @@ using namespace std;
 // Constructor
 BorrowCmd::BorrowCmd() : cmd('B') {} 
 
+// helper to borrow movie
+
 /*
   Give movie to customer and update stock
 */
 void BorrowCmd::execute(MovieStore &store, const vector<string> &vs) const {
-    // vs[0] = B id D genre title
-    // vs[1] = year
     char cmd;
-    int id;
     char type;
     char genre;
+    int id;
+    int month;
+    int year;
     string title;
-    int year = stoi(vs[1]);
-    // Use sstream to get variables separated by spaces in vs[0]
+    string director;
+    string majorActor;
+
+    // vs[0] = cmd id type genre
     istringstream iss(vs[0]);
     iss >> cmd >> id >> type >> genre;
-    // Get full title (rest of iss)
-    getline(iss, title);
-    title = title.substr(title.find_first_not_of(' ')); // remove leading space
 
     // Get inventory and customerlist
     const auto &inventory = store.getMoviesByType();
     const auto &customers = store.getCustomerList();
     
     // Get customer
-    Customer *customer = customers.at(id);
+    Customer *customer = nullptr;
+    auto it = customers.find(id);
+    if (it != customers.end()) {
+        // Save valid customer
+        customer = it->second;
+    }
 
-    cout << title << ", " << year << "     <-- 38:borrowCmd.cpp" << endl;
+    if (genre == 'F') {
+        // Check for Valid Type
+        if (type != 'D') {
+            // output: Invalid media type <type>, discarding line:  F Fargo, 1996
+            string str;
+            getline(iss, str);
+            cout << "Invalid media type " << type << ", discarding line: " << 
+            str << ", " << vs[1] << endl;
+            return;
+        }
+
+        // Check for valid customer
+        if (customer == nullptr) {
+            // Invalid Customer
+            // output: Invalid customer ID <id>, discarding line:  D F You've Got Mail, 1998"
+            istringstream iss(vs[0]);
+            string str;
+            iss >> cmd >> id;
+            getline(iss, str);
+            cout << "Invalid customer ID " << id << ", discarding line: " << 
+            str << ", " << vs[1] << endl;
+            return;
+        }
+
+        // str = cmd id type genre title, year
+        // vs[0] = B id D F title
+        // vs[1] = year
+        year = stoi(vs[1]);
+        // Get full title (rest of iss)
+        getline(iss, title);
+        title = title.substr(title.find_first_not_of(' ')); // remove leading space
+        cout << title << ", " << year << "     <-- 49:borrowCmd.cpp" << endl;
+    }
+
+    if (genre == 'D') {
+        // Check for Valid Type
+        if (type != 'D') {
+            // output: Invalid media type <type>, discarding line:  D director, title
+            string str;
+            getline(iss, str);
+            cout << "Invalid media type " << type << ", discarding line: " << 
+            str << ", " << vs[1] << endl;
+            return;
+        }
+        
+        // Check for valid customer
+        if (customer == nullptr) {
+            // Invalid Customer
+            // output: Invalid customer ID <id>, discarding line:  D D director, title
+            istringstream iss(vs[0]);
+            string str;
+            iss >> cmd >> id;
+            getline(iss, str);
+            cout << "Invalid customer ID " << id << ", discarding line: " << 
+            str << ", " << vs[1] << endl;
+            return;
+        }
+
+        // str = cmd id type genre director, title
+        // vs[0] = B id D D director
+        // vs[1] = title
+        title = vs[1];
+        getline(iss, director);
+        director = director.substr(director.find_first_not_of(' ')); // remove leading space
+        cout << title << ", " << director << "     <-- 106:borrowCmd.cpp" << endl;
+    }
+
+    if (genre == 'C') { // todo
+        // Check for Valid Type
+        if (type != 'D') {
+            // output: Invalid media type <type>, discarding line:  C 5 1940 Katherine Hepburn
+            string str;
+            getline(iss, str);
+            cout << "Invalid media type " << type << ", discarding line: " << 
+            str << ", " << vs[1] << endl;
+            return;
+        }
+
+        // Check for valid customer
+        if (customer == nullptr) {
+            // Invalid Customer
+            // output: Invalid customer ID <id>, discarding line:  D C release date director
+            istringstream iss(vs[0]);
+            string str;
+            iss >> cmd >> id;
+            getline(iss, str);
+            cout << "Invalid customer ID " << id << ", discarding line: " << 
+            str << endl;
+            return;
+        }
+
+        // str = cmd id type genre month year firstName lastName
+        // vs[0] = B id D C month year firstName lastName
+        // Use sstream to get all variables
+        string firstName;
+        string lastName;
+        iss >> month >> year >> firstName >> lastName;
+        majorActor = firstName + " " + lastName;
+        cout << month << " " << year << " " << majorActor << "     <-- 127:borrowCmd.cpp" << endl;
+    }
+
     // Get movie
-    Movie *movieToBorrow;
+    Movie *movieToBorrow = nullptr;
     const auto &movieList = inventory[genre - 'A'];
     if (!movieList.empty()) {
         for (Movie *movie : movieList) {
-            if (movie->title == title && movie->getYear() == year) {
-            cout << movie->title << ", " << movie->getYear() << "     <-- 44:borrowCmd.cpp" << endl;
+            // Comedy
+            if (genre == 'F' && (movie->title == title && movie->getYear() == year)) {
+            cout << movie->title << ", " << movie->getYear() << "     <-- 137:borrowCmd.cpp" << endl;
             movieToBorrow = movie;
+            break;
             }
+            
+            // Drama
+            if (genre == 'D' && (movie->title == title && movie->director == director)) {
+            cout << movie->title << ", " << movie->director << "     <-- 143:borrowCmd.cpp" << endl;
+            movieToBorrow = movie;
+            break;
+            }
+
+            // Classic (todo)
+            
         }
     }
+    
+    // Check if movie is invalid (bogus title) (todo: output)
+    if (movieToBorrow == nullptr) {
+        // output: Invalid movie  for customer <fullName>, discarding line:"
+        cout << "Invalid movie  for customer " << customer->getFullName() << 
+        ", discarding line: " << endl;
+        return;
+    }
 
-    // borrow movie
-    customer->borrowMovie(movieToBorrow);
-
-    // update stock
-    movieToBorrow->stock--;
-
-    // test print 
-    movieToBorrow->print(); cout << "^-- 56:borrowCmd.cpp" << endl;
+    // Check stock
+    if (movieToBorrow->stock > 0) {
+        // Borrow movie
+        customer->borrowMovie(movieToBorrow); 
+        // output: Debug: Borrow <id> <fullName> movie->print()
+        cout << "Debug: Borrow " << id << " " << customer->getFullName() << " ";
+        movieToBorrow->print();
+        movieToBorrow->stock--; // Update stock
+    } else {
+        // output: 
+        // ==========================
+        // <fullName> could NOT borrow <title>, out of stock: 
+        // ==========================
+        // Failed to execute command: Borrow <fullName> <title>
+    }
 }
 
 BorrowCmdFactory::BorrowCmdFactory() { registerType('B', this); }
 
 Command *BorrowCmdFactory::makeCommand(const vector<string> &vs) const {
-    assert(vs.size() == 2); // to avoid unused parameter warning  
+    assert(vs.size() == 2 || 1); // to avoid unused parameter warning  
     return new BorrowCmd();
 }
 
