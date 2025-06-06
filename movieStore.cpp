9c3ca2ac99e06f776bd07da53e82fb76d5fb2a32
Kwright6 <usaf633@yahoo.com>
2025-06-06 10:17:51 +0000

    formatting output

--- movieStore.cpp
+++ movieStore.cpp
@@ -117,7 +117,7 @@ void MovieStore::readCommandsFromFile(const string &filename) {
     if (command != nullptr) { // ensure command was recognized
       // execute command and pass a reference to this store
       command->execute(*this, vs);
-      cout << "==========================" << endl;
+      //cout << "==========================" << endl;
     }
     delete command;
   }
