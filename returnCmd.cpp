7dca5f9c95a02e98c9f52990133a7f9e78d1daca
Kwright6 <usaf633@yahoo.com>
2025-06-06 11:46:00 +0000

    Removed Debug Lines

--- returnCmd.cpp
+++ returnCmd.cpp
@@ -104,7 +104,7 @@ void ReturnCmd::execute(MovieStore &store, const vector<string> &vs) const {
   // Return movie
   // output: Debug: Return <id> <fullName> movie->print()
   //cout << "Debug: Return " << id << " " << customer->getFullName() << " ";
-  movieToReturn->print();
+  //movieToReturn->print();
   customer->returnMovie(movieToReturn);
   movieToReturn->stock++; // Update stock
 }
