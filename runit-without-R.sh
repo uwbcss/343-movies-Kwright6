a8a6a99684a6dc055cc6027069ce57786dd3f43f
Kwright6 <usaf633@yahoo.com>
2025-06-06 13:08:02 +0000

    added runit without returnCmd.cpp

---
+++ runit-without-R.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Compiling WITHOUT R..."
+
+g++ -g -DEXCLUDE_RETURNCMD \
+  main.cpp borrowCmd.cpp classics.cpp comedy.cpp command.cpp \
+  customer.cpp drama.cpp historyCmd.cpp inventoryCmd.cpp \
+  movie.cpp movieStore.cpp returnCmd.cpp store_test.cpp \
+  -o my_program
+
+if [ $? -eq 0 ]; then
+  echo "Compilation successful, running program..."
+  ./my_program
+else
+  echo "Compilation failed."
+fi
